ff93a3441b73ad48542b2800135eb1f9a9844eb4
tractortoby <tobychung1993@gmail.com>
2022-01-17 16:09:38 +0800

    Add Intl.NumberFormat() constructor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Button, Container, Stack } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
+
 function App() {
-  return <div className='App'>Learn React</div>;
+  return (
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap='2' className='mb-4'>
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}
+      >
+        <BudgetCard></BudgetCard>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
